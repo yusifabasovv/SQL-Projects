0d5d409a313bed88cddc3f75c8753f90ac456ef0
Yusif <67735529+yusifabasovv@users.noreply.github.com>
2021-09-25 23:52:32 +0400

    Create Solutions.sql

---
+++ Project 2/Solutions.sql
@@ -0,0 +1,24 @@
+
+
+## Q1 :
+
+SELECT ROW_NUMBER() OVER(PARTITION BY custid
+       ORDER BY freight ) AS Sequences,orderid as Order_ID from sales.Orders
+
+## Q2 :
+
+select quantity as Quantity ,
+case
+when quantity=(select avg(quantity) from production.stocks) then 'Stock Normal'
+when quantity>(select avg(quantity) from production.stocks) then 'Stock Normal'
+else 'Stock go out'
+end as [Stock Condition]
+
+from production.stocks
+
+## Q3 :	
+
+select p.product_name ,sum(o.list_price-o.discount) as Spending,sum(o.quantity) as [Stock Needed] from sales.order_items o,production.products p
+ where o.product_id=p.product_id group by p.product_name order by Spending,[Stock Needed]
+
+
